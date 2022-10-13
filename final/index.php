ddf3ae7335fec7d207b3fac13689ba9e8a44230c
cindyqquach <91432784+cindyqquach@users.noreply.github.com>
2022-10-13 02:06:41 -0400

    First commit - HW1

---
+++ final/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Idm232 - Alpha</title>
+</head>
+<body>
+    <?php include_once 'includes/header.php'; ?>
+    <?php $title = "Assignment - 01" ?>
+    <h1><?php echo $title; ?></h1>
+    <?php include_once 'includes/footer.php'; ?>
+</body>
+</html>
